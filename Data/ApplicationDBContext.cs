61eb5459b274577fa4feee27ed29ff9079acf37e
tuliobit <hagano@duck.com>
2024-05-15 15:41:27 -0300

    Adicionando contexto, SQL Server Express - Migrations para o model Todo

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
+using TodoAPI.Models;
+
+namespace TodoAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
