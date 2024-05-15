a9d1e13603f0ed70cf81f5582f4302517b5c27d3
tuliobit <hagano@duck.com>
2024-05-15 14:06:57 -0300

    Add project files.

---
+++ Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
